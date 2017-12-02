f7b16ec144cde25147ad264312e42b79ef6f6e98
9954316 <9954316@qq.com>
2017-12-02 22:22:12 +0800

    3rd week

---
+++ third_week/lab-19/shiyanlou/pipelines.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy.orm import sessionmaker
+from shiyanlou.models import Course,engine
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class ShiyanlouPipeline(object):
+
+    def process_item(self, item, spider):
+        item['students']=int(item['students'])
+        self.session.add(Course(**item))
+        return item
+
+    def open_spider(self,spider):
+
+        Session=sessionmaker(bind=engine)
+        self.session=Session()
+
+    def close_spider(self,spider):
+
+        self.session.commit()
+        self.session.close()
